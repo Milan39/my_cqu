d73dd59d766433d8af94a01658811f5ff03d41e6
Milan Tilija <milantilja39@gmail.com>
2025-03-30 12:56:14 +1100

    changes in login

--- lib/feature/auth/cubit/login_cubit.dart
+++ lib/feature/auth/cubit/login_cubit.dart
@@ -7,26 +7,40 @@ part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   final GlobalKey<FormBuilderState> formKey = GlobalKey<FormBuilderState>();
+  final FocusNode emailFocusNode = FocusNode();
+  final FocusNode passwordFocusNode = FocusNode();
 
   LoginCubit() : super(LoginState());
 
   Future<void> loginUser() async {
     emit(state.copyWith(loginStatus: LoginStatus.loading));
+    await Future.delayed(Duration(seconds: 3));
     try {
       if (formKey.currentState!.isValid) {
         final String username = formKey.currentState?.value['username'];
         final String password = formKey.currentState?.value['password'];
-        if (username.toLowerCase() == 'milan.tilija@cqumail.com' && password == "root1234") {
+        if (username.toLowerCase() == 'milan.tilija@cqumail.com' &&
+            password == "root1234") {
           emit(state.copyWith(loginStatus: LoginStatus.success));
         } else {
+          for (var field in ['username', 'password']) {
+            formKey.currentState?.fields[field]?.invalidate(
+              "Invalid email or password",
+            );
+          }
           emit(state.copyWith(loginStatus: LoginStatus.failure));
         }
-
       }
     } catch (e) {
       emit(state.copyWith(loginStatus: LoginStatus.failure));
     }
   }
+
+  // void handleOnChanged({bool isPassword = true}) {
+  //   final field = isPassword ? 'password' : 'username';
+  //   final isFieldValid = formKey.currentState?.fields['field']?.validate();
+  //
+  // }
 
   void togglePassword() {
     emit(state.copyWith(hidePassword: !state.hidePassword));
